2d45a451cf41bbca37ff716859e35b23bae15060
jomosque <jonjfm@hotmail.com>
2017-09-20 18:43:11 -0500

    Este es el primer commit de Grawi 21

---
+++ app/src/main/java/com/example/jomosque/facebooklogin/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.jomosque.facebooklogin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    LoginButton loginButton;
+    TextView textView;
+
+    CallbackManager callbackManager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.fb_login_bn);
+        textView = (TextView)findViewById(R.id.textView);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textView.setText("Login Succeed \n" +
+                loginResult.getAccessToken().getUserId() + "\n" +
+                loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {                  textView.setText("Login Error");
+            }
+        });
+
+    }
+
+    protected void onActivyResult(int requestCode, int resultCode, Intent data){
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
